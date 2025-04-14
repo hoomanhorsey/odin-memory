5804aecd92be42a175cb3bf3b32c440d3ae8d8b1
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2025-04-14 23:11:02 +1000

    Display initial gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="gameBoard">
         <GameBoardRender array={array} />
       </div>
 
--- src/components/gameBoard.jsx
+++ src/components/gameBoard.jsx
@@ -3,11 +3,19 @@ import { getRandomArray } from "./randomArray";
 const array = getRandomArray();
 
 console.log(array);
+function handleCardClick(e) {
+  console.log(e.target.id);
+}
 
 function GameBoardRender({ array }) {
   const board = [];
   for (let i = 0; i < array.length; i++) {
-    board.push(<div className="gameCard"> {array[i]} </div>);
+    board.push(
+      <div className="gameCard" id={array[i]} onClick={handleCardClick}>
+        {" "}
+        Picture of cat and: {array[i]}{" "}
+      </div>
+    );
   }
   console.log(board[0]);
   return <>{board}</>;
